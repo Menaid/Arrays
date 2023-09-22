3901657910b4a487d70f5fab057c8c5019ebc6f8
Menaid <menaidnocic@live.se>
2023-09-22 10:58:30 +0200

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿string[] lines = File.ReadAllLines("../../../data.txt");
+
+foreach (string line in lines)
+{
+    Console.WriteLine(line);
+}
+
+if(lines.Length >= 2)
+{
+    Console.WriteLine(lines[1]);
+}
+
